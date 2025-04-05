740c8551ac08165cfcb6c5caaf22277d538bf910
eagerbeaver04 <mike.markov04@gmail.com>
2025-04-05 19:18:04 +0300

    fix: clang format:

--- lab8/data_structures.h
+++ lab8/data_structures.h
@@ -1,155 +1,117 @@
-#include <d3d11.h>
-#include <d3dcompiler.h>
-#include <cstdlib>
-#include <ctime>
-#include <cmath>
-#include <windows.h>
-#include <DirectXMath.h>
 #include "DDSTextureLoader.h"
 #include "imgui/imgui.h"
-#include "imgui/imgui_impl_win32.h"
 #include "imgui/imgui_impl_dx11.h"
+#include "imgui/imgui_impl_win32.h"
+#include <DirectXMath.h>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <windows.h>
 
 using namespace DirectX;
 
-
 const int MAX_CUBES_GPU = 20;
 
-struct CullingData
-{
-    UINT numShapes;
-    UINT padding[3];
-    XMFLOAT4 bbMin[MAX_CUBES_GPU];
-    XMFLOAT4 bbMax[MAX_CUBES_GPU];
+struct CullingData {
+  UINT numShapes;
+  UINT padding[3];
+  XMFLOAT4 bbMin[MAX_CUBES_GPU];
+  XMFLOAT4 bbMax[MAX_CUBES_GPU];
 };
 
-struct Plane
-{
-    float a, b, c, d;
+struct Plane {
+  float a, b, c, d;
 };
 
-struct SimpleVertex
-{
-    float x, y, z;
-    float nx, ny, nz;
-    float u, v;
+struct SimpleVertex {
+  float x, y, z;
+  float nx, ny, nz;
+  float u, v;
 };
-struct SceneCBData
-{
-    XMFLOAT4X4 viewProjectionMatrix;
-    XMFLOAT4 planes[6];
+struct SceneCBData {
+  XMFLOAT4X4 viewProjectionMatrix;
+  XMFLOAT4 planes[6];
 };
 
+SimpleVertex g_CubeVertices[] = {
 
-SimpleVertex g_CubeVertices[] =
-{
+    {-0.5f, -0.5f, 0.5f, 0, 0, 1, 0.0f, 1.0f},
+    {0.5f, -0.5f, 0.5f, 0, 0, 1, 1.0f, 1.0f},
+    {0.5f, 0.5f, 0.5f, 0, 0, 1, 1.0f, 0.0f},
+    {-0.5f, -0.5f, 0.5f, 0, 0, 1, 0.0f, 1.0f},
+    {0.5f, 0.5f, 0.5f, 0, 0, 1, 1.0f, 0.0f},
+    {-0.5f, 0.5f, 0.5f, 0, 0, 1, 0.0f, 0.0f},
 
-    { -0.5f, -0.5f,  0.5f,    0,0,1,   0.0f, 1.0f },
-    {  0.5f, -0.5f,  0.5f,    0,0,1,   1.0f, 1.0f },
-    {  0.5f,  0.5f,  0.5f,    0,0,1,   1.0f, 0.0f },
-    { -0.5f, -0.5f,  0.5f,    0,0,1,   0.0f, 1.0f },
-    {  0.5f,  0.5f,  0.5f,    0,0,1,   1.0f, 0.0f },
-    { -0.5f,  0.5f,  0.5f,    0,0,1,   0.0f, 0.0f },
+    {0.5f, -0.5f, -0.5f, 0, 0, -1, 0.0f, 1.0f},
+    {-0.5f, -0.5f, -0.5f, 0, 0, -1, 1.0f, 1.0f},
+    {-0.5f, 0.5f, -0.5f, 0, 0, -1, 1.0f, 0.0f},
+    {0.5f, -0.5f, -0.5f, 0, 0, -1, 0.0f, 1.0f},
+    {-0.5f, 0.5f, -0.5f, 0, 0, -1, 1.0f, 0.0f},
+    {0.5f, 0.5f, -0.5f, 0, 0, -1, 0.0f, 0.0f},
 
-    {  0.5f, -0.5f, -0.5f,    0,0,-1,  0.0f, 1.0f },
-    { -0.5f, -0.5f, -0.5f,    0,0,-1,  1.0f, 1.0f },
-    { -0.5f,  0.5f, -0.5f,    0,0,-1,  1.0f, 0.0f },
-    {  0.5f, -0.5f, -0.5f,    0,0,-1,  0.0f, 1.0f },
-    { -0.5f,  0.5f, -0.5f,    0,0,-1,  1.0f, 0.0f },
-    {  0.5f,  0.5f, -0.5f,    0,0,-1,  0.0f, 0.0f },
+    {-0.5f, -0.5f, -0.5f, -1, 0, 0, 0.0f, 1.0f},
+    {-0.5f, -0.5f, 0.5f, -1, 0, 0, 1.0f, 1.0f},
+    {-0.5f, 0.5f, 0.5f, -1, 0, 0, 1.0f, 0.0f},
+    {-0.5f, -0.5f, -0.5f, -1, 0, 0, 0.0f, 1.0f},
+    {-0.5f, 0.5f, 0.5f, -1, 0, 0, 1.0f, 0.0f},
+    {-0.5f, 0.5f, -0.5f, -1, 0, 0, 0.0f, 0.0f},
 
-    { -0.5f, -0.5f, -0.5f,   -1,0,0,   0.0f, 1.0f },
-    { -0.5f, -0.5f,  0.5f,   -1,0,0,   1.0f, 1.0f },
-    { -0.5f,  0.5f,  0.5f,   -1,0,0,   1.0f, 0.0f },
-    { -0.5f, -0.5f, -0.5f,   -1,0,0,   0.0f, 1.0f },
-    { -0.5f,  0.5f,  0.5f,   -1,0,0,   1.0f, 0.0f },
-    { -0.5f,  0.5f, -0.5f,   -1,0,0,   0.0f, 0.0f },
+    {0.5f, -0.5f, 0.5f, 1, 0, 0, 0.0f, 1.0f},
+    {0.5f, -0.5f, -0.5f, 1, 0, 0, 1.0f, 1.0f},
+    {0.5f, 0.5f, -0.5f, 1, 0, 0, 1.0f, 0.0f},
+    {0.5f, -0.5f, 0.5f, 1, 0, 0, 0.0f, 1.0f},
+    {0.5f, 0.5f, -0.5f, 1, 0, 0, 1.0f, 0.0f},
+    {0.5f, 0.5f, 0.5f, 1, 0, 0, 0.0f, 0.0f},
 
-    {  0.5f, -0.5f,  0.5f,    1,0,0,   0.0f, 1.0f },
-    {  0.5f, -0.5f, -0.5f,    1,0,0,   1.0f, 1.0f },
-    {  0.5f,  0.5f, -0.5f,    1,0,0,   1.0f, 0.0f },
-    {  0.5f, -0.5f,  0.5f,    1,0,0,   0.0f, 1.0f },
-    {  0.5f,  0.5f, -0.5f,    1,0,0,   1.0f, 0.0f },
-    {  0.5f,  0.5f,  0.5f,    1,0,0,   0.0f, 0.0f },
+    {-0.5f, 0.5f, 0.5f, 0, 1, 0, 0.0f, 1.0f},
+    {0.5f, 0.5f, 0.5f, 0, 1, 0, 1.0f, 1.0f},
+    {0.5f, 0.5f, -0.5f, 0, 1, 0, 1.0f, 0.0f},
+    {-0.5f, 0.5f, 0.5f, 0, 1, 0, 0.0f, 1.0f},
+    {0.5f, 0.5f, -0.5f, 0, 1, 0, 1.0f, 0.0f},
+    {-0.5f, 0.5f, -0.5f, 0, 1, 0, 0.0f, 0.0f},
 
-    { -0.5f,  0.5f,  0.5f,    0,1,0,   0.0f, 1.0f },
-    {  0.5f,  0.5f,  0.5f,    0,1,0,   1.0f, 1.0f },
-    {  0.5f,  0.5f, -0.5f,    0,1,0,   1.0f, 0.0f },
-    { -0.5f,  0.5f,  0.5f,    0,1,0,   0.0f, 1.0f },
-    {  0.5f,  0.5f, -0.5f,    0,1,0,   1.0f, 0.0f },
-    { -0.5f,  0.5f, -0.5f,    0,1,0,   0.0f, 0.0f },
-
-    { -0.5f, -0.5f, -0.5f,    0,-1,0,  0.0f, 1.0f },
-    {  0.5f, -0.5f, -0.5f,    0,-1,0,  1.0f, 1.0f },
-    {  0.5f, -0.5f,  0.5f,    0,-1,0,  1.0f, 0.0f },
-    { -0.5f, -0.5f, -0.5f,    0,-1,0,  0.0f, 1.0f },
-    {  0.5f, -0.5f,  0.5f,    0,-1,0,  1.0f, 0.0f },
-    { -0.5f, -0.5f,  0.5f,    0,-1,0,  0.0f, 0.0f },
+    {-0.5f, -0.5f, -0.5f, 0, -1, 0, 0.0f, 1.0f},
+    {0.5f, -0.5f, -0.5f, 0, -1, 0, 1.0f, 1.0f},
+    {0.5f, -0.5f, 0.5f, 0, -1, 0, 1.0f, 0.0f},
+    {-0.5f, -0.5f, -0.5f, 0, -1, 0, 0.0f, 1.0f},
+    {0.5f, -0.5f, 0.5f, 0, -1, 0, 1.0f, 0.0f},
+    {-0.5f, -0.5f, 0.5f, 0, -1, 0, 0.0f, 0.0f},
 };
 
-struct FullScreenVertex
-{
-    float x, y, z, w;
-    float u, v;
+struct FullScreenVertex {
+  float x, y, z, w;
+  float u, v;
 };
 
-FullScreenVertex g_FullScreenTriangle[3] =
-{
-    { -1.0f, -1.0f, 0, 1,   0.0f, 1.0f },
-    { -1.0f,  3.0f, 0, 1,   0.0f,-1.0f },
-    {  3.0f, -1.0f, 0, 1,   2.0f, 1.0f }
+FullScreenVertex g_FullScreenTriangle[3] = {{-1.0f, -1.0f, 0, 1, 0.0f, 1.0f},
+                                            {-1.0f, 3.0f, 0, 1, 0.0f, -1.0f},
+                                            {3.0f, -1.0f, 0, 1, 2.0f, 1.0f}};
+
+struct SkyboxVertex {
+  float x, y, z;
 };
 
-struct SkyboxVertex
-{
-    float x, y, z;
+SkyboxVertex g_SkyboxVertices[] = {
+    {-1.0f, -1.0f, -1.0f}, {-1.0f, 1.0f, -1.0f},  {1.0f, 1.0f, -1.0f},
+    {-1.0f, -1.0f, -1.0f}, {1.0f, 1.0f, -1.0f},   {1.0f, -1.0f, -1.0f},
+    {1.0f, -1.0f, 1.0f},   {1.0f, 1.0f, 1.0f},    {-1.0f, 1.0f, 1.0f},
+    {1.0f, -1.0f, 1.0f},   {-1.0f, 1.0f, 1.0f},   {-1.0f, -1.0f, 1.0f},
+    {-1.0f, -1.0f, 1.0f},  {-1.0f, 1.0f, 1.0f},   {-1.0f, 1.0f, -1.0f},
+    {-1.0f, -1.0f, 1.0f},  {-1.0f, 1.0f, -1.0f},  {-1.0f, -1.0f, -1.0f},
+    {1.0f, -1.0f, -1.0f},  {1.0f, 1.0f, -1.0f},   {1.0f, 1.0f, 1.0f},
+    {1.0f, -1.0f, -1.0f},  {1.0f, 1.0f, 1.0f},    {1.0f, -1.0f, 1.0f},
+    {-1.0f, 1.0f, -1.0f},  {-1.0f, 1.0f, 1.0f},   {1.0f, 1.0f, 1.0f},
+    {-1.0f, 1.0f, -1.0f},  {1.0f, 1.0f, 1.0f},    {1.0f, 1.0f, -1.0f},
+    {-1.0f, -1.0f, 1.0f},  {-1.0f, -1.0f, -1.0f}, {1.0f, -1.0f, -1.0f},
+    {-1.0f, -1.0f, 1.0f},  {1.0f, -1.0f, -1.0f},  {1.0f, -1.0f, 1.0f},
 };
 
-SkyboxVertex g_SkyboxVertices[] =
-{
-    { -1.0f, -1.0f, -1.0f },
-    { -1.0f,  1.0f, -1.0f },
-    {  1.0f,  1.0f, -1.0f },
-    { -1.0f, -1.0f, -1.0f },
-    {  1.0f,  1.0f, -1.0f },
-    {  1.0f, -1.0f, -1.0f },
-    {  1.0f, -1.0f,  1.0f },
-    {  1.0f,  1.0f,  1.0f },
-    { -1.0f,  1.0f,  1.0f },
-    {  1.0f, -1.0f,  1.0f },
-    { -1.0f,  1.0f,  1.0f },
-    { -1.0f, -1.0f,  1.0f },
-    { -1.0f, -1.0f,  1.0f },
-    { -1.0f,  1.0f,  1.0f },
-    { -1.0f,  1.0f, -1.0f },
-    { -1.0f, -1.0f,  1.0f },
-    { -1.0f,  1.0f, -1.0f },
-    { -1.0f, -1.0f, -1.0f },
-    { 1.0f, -1.0f, -1.0f },
-    { 1.0f,  1.0f, -1.0f },
-    { 1.0f,  1.0f,  1.0f },
-    { 1.0f, -1.0f, -1.0f },
-    { 1.0f,  1.0f,  1.0f },
-    { 1.0f, -1.0f,  1.0f },
-    { -1.0f, 1.0f, -1.0f },
-    { -1.0f, 1.0f,  1.0f },
-    { 1.0f, 1.0f,  1.0f },
-    { -1.0f, 1.0f, -1.0f },
-    { 1.0f, 1.0f,  1.0f },
-    { 1.0f, 1.0f, -1.0f },
-    { -1.0f, -1.0f,  1.0f },
-    { -1.0f, -1.0f, -1.0f },
-    { 1.0f, -1.0f, -1.0f },
-    { -1.0f, -1.0f,  1.0f },
-    { 1.0f, -1.0f, -1.0f },
-    { 1.0f, -1.0f,  1.0f },
-};
-
-struct LightBufferType
-{
-    XMFLOAT4 light0Pos;
-    XMFLOAT4 light0Color;
-    XMFLOAT4 light1Pos;
-    XMFLOAT4 light1Color;
-    XMFLOAT4 ambient;
+struct LightBufferType {
+  XMFLOAT4 light0Pos;
+  XMFLOAT4 light0Color;
+  XMFLOAT4 light1Pos;
+  XMFLOAT4 light1Color;
+  XMFLOAT4 ambient;
 };
